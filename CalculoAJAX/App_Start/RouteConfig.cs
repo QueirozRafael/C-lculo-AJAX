6c75de23393843ea82895b6b7bb6be2efb38ba1f
queiroz.rafaelreis@gmail.com <queiroz.rafaelreis@gmail.com>
2017-12-20 02:30:10 -0200

    Primeiro Commit

--- CalculoAJAX/App_Start/RouteConfig.cs
+++ CalculoAJAX/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CalculoAJAX
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "AJAX", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ CalculoAJAX/Controllers/AJAXController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculoAJAX.Controllers
+{
+    public class AJAXController : Controller
+    {
+        // GET: AJAX
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult JsonFatorial(int n)
+        {
+            return new JsonResult
+            {
+                Data = new { Fatorial = Fatorial(n) }
+            };
+        }
+
+        private object Fatorial(int n)
+        {
+            var numero = (n == 0 ? 0 : n);
+
+            for (int i = 2; i < n; i++)
+            {
+                numero *= i;
+            }
+
+            return numero;
+        }
+    }
+}
